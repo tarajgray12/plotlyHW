0e404343b38cc13711cf0e196fafeb772dd2034d
Tgray <tarajgray12@gmail.com>
2019-03-31 15:15:17 -0700

    hw

--- static/app.js
+++ static/app.js
@@ -1,69 +1,73 @@
 function buildMetadata(sample) {
 
   // @TODO: Complete the following function that builds the metadata panel
-
+  
   // Use `d3.json` to fetch the metadata for a sample
+    var url =`/metadata/${sample}`
+    d3.json(url).then(function(data){
+  
     // Use d3 to select the panel with id of `#sample-metadata`
-    url=`/metadata/${sample}`
-
-    d3.json(url).then(function(data) {
-      newFunction(data);
-      //console.log(data.WFREQ)
- 
-    // Use `.html("") to clear any existing metadata
-
+    var metaData = d3.select("#sample-metadata");
+    metaData.html("");
     // Use `Object.entries` to add each key and value pair to the panel
+    Object.entries(data).forEach(([key, value]) => {
+      // metadata.append.text([key, value]);
+      metaData.append("h6").text(`${key}:  ${value}`);
+    });
+  });
+}
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-    })
-};
 
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
-  url=`/samples/${sample}`
-  d3.json(url).then(function(sample_data) {
-    
+  url = `/samples/${sample}`
+  d3.json(url).then(function(sampleData) {
     // @TODO: Build a Bubble Chart using the sample data
-    var trace1 = {
-      x: sample_data.otu_ids, 
-      y: sample_data.sample_values,
+    var bubble = {
+      x: sampleData.otu_ids,
+      y: sampleData.sample_values,
       mode: "markers",
       marker: {
-         color: sample_data.otu_ids,
-         size: sample_data.sample_values
+        color: sampleData.otu_ids,
+        size: sampleData.sample_values,
       },
-      text: sample_data.otu_labels
+      text: sampleData.otu_lables
     };
-    
     var layout = {
       title: "Bubble chart",
       xaxis: {title: 'OTU ID'},
       showlegend: false,
     };
+    var bubbleData = [bubble]
+    Plotly.newPlot("bubble", bubbleData, layout)
 
-    var trace = [trace1]
-    Plotly.newPlot("bubble", trace, layout)
     // @TODO: Build a Pie Chart
+    var data = [];
+    for (var i=0; i<sampleData.otu_ids.length; i++) {
+      data.push({'id': sampleData.otu_ids[i], 'label': sampleData.otu_labels[i], 'value': sampleData.sample_values[i]});
+    }
+    data.sort(function(a, b){return b.value-a.value})
+    var slicedData= data.slice(0, 10);
+
+    var pie = {
+      "labels": slicedData.map(sd=> sd.id),
+      "values": slicedData.map(sd => sd.value),
+      "type": "pie",
+      "hovertext": slicedData.map(sd => sd.label)
+    };
+
+    var pieData= [pie];
+    console.log(slicedData);
+    Plotly.newPlot("pie", pieData);
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
-    console.log(sample_data.otu_labels.slice(0, 10))
-    tracepie = {
-      "values" : sample_data.sample_values.slice(0, 10),
-      "labels" : sample_data.otu_ids.slice(0, 10),
-      "name" : sample_data.otu_labels.slice(0, 10),
-      "hovertext": sample_data.otu_labels.slice(0, 10),
-      "type" : "pie"
-    };
-    layout = {
-      // hoverinfo: "name",
-      title: "Top 10 details"
-    }
-    data = [tracepie]
-    Plotly.newPlot("pie", data)
+    
+
   });
 }
 
